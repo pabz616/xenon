5911ac6445457a45476907491e946d34f90b0d59
Pablo Vergara <vergara.pablo0616@gmail.com>
2024-06-10 16:38:01 -0400

    setup for playwright with java

---
+++ firsttest/src/main/java/com/playwrightTest/FirstPlaywrightTest.java
@@ -0,0 +1,15 @@
+package com.playwrightTest;
+
+import com.microsoft.playwright.*;
+
+public class FirstPlaywrightTest {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+            System.out.println(page.title());
+            browser.close();
+        }
+    }
+}
